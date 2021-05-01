1b0298a1671f63543fe0d87cb14069859ef8f7b2
Mitchell R <support@bettercorp.co.za>
2021-05-02 01:38:33 +0200

    adding missing file

---
+++ src/plugins/ucrm/sec.config.ts
@@ -0,0 +1,11 @@
+import * as crypto from 'crypto';
+import { IUCRMPluginConfig } from '../../weblib';
+
+export default (): IUCRMPluginConfig => {
+  return {
+    webhooks: false,
+    crmAPI: false,
+    clientKey: crypto.pseudoRandomBytes(64).toString('hex'),
+    myHost: 'http://localhost'
+  };
+};
